f56045ff639713fdcf3c54de852ab70bf32653b0
Gabrielviscio10 <gonzalo.viscio@icloud.com>
2024-08-25 12:12:47 +0000

    css a la mitad

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,12 +1,13 @@
-
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-
 import { Context } from "../store/appContext";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan,faPen,faLocationDot,faEnvelope,faPhoneFlip } from '@fortawesome/free-solid-svg-icons';
 
-export const Home = () =>  {
+import "../../styles/home.css";
+import gatolentes from "../../img/gatolentes.jpg";
+
+export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
@@ -18,21 +19,35 @@ export const Home = () =>  {
 							key={index}
 							className="list-group-item d-flex justify-content-between"
 							style={{ background: item.background || "white" }}>
-							<div>
-								<span>{item.name}</span>
-								<br />
-								<span>{item.address}</span>
-								<br />
-								<span>{item.phone}</span>
-								<br/>
-								<span>{item.id}</span>
+							<div className="d-flex justify-content-between">
+								<div className="">
+									<img 
+										src={gatolentes} 
+										alt="Descripción de la imagen" 
+										style={{
+											objectFit: 'cover',
+											overflow: 'hidden', 
+											width: '120px', 
+											height: '120px', 
+											borderRadius: '50%' 
+										}} 
+									/>
+								</div>
+								<div>
+									<p>{item.name}</p>
+									<p><FontAwesomeIcon icon={faLocationDot} />{item.address}</p>
+									<p><FontAwesomeIcon icon={faPhoneFlip} />{item.phone}</p>
+									<p><FontAwesomeIcon icon={faEnvelope} />{item.email}</p>
+								</div>
 							</div>
-							<Link to={`/editContact/${item.id}`}>
-								<button className="btn btn-primary">Edit</button>
-							</Link>	
-							<button className="btn btn-danger" onClick={() => actions.deleteContact(index)}>
-								Eliminar
-							</button>
+							<div style={{marginRight:'20px', marginTop:'15px',}}>
+								<Link to={`/editContact/${item.id}`}>
+									<button className="btn btn-link text-dark"><FontAwesomeIcon icon={faPen} /></button>
+								</Link>
+								<button className="btn btn-link text-dark" onClick={() => actions.deleteContact(index)}>
+									<FontAwesomeIcon icon={faTrashCan} />
+								</button>
+							</div>
 					    </li>
 					);
 				})}
